4a11204ad156e1cc347fedb5236b49e51501f601
Mark Buckner <buckner.a.mark@gmail.com>
2021-05-10 11:08:36 -0600

    Create console.el

---
+++ console.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `lisp-interaction-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'lisp-interaction-mode
+		     '(("con" "console.log(${var})\n" "console log" nil nil nil "/Users/developer/.emacs.d/snippets/lisp-interaction-mode/+new-snippet+" nil nil)))
+
+
+;;; Do not edit! File generated at Sun Oct 18 13:16:00 2020
